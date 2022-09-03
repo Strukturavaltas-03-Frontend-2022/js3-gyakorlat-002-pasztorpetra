c3c5e1d209e36e87bbec6a677d609921a737029a
Petra <pasztor.petra1998@gmail.com>
2022-09-03 12:14:58 +0200

    1.kész

--- 09-storage/exercises/02/solution/js/cookieHandler.js
+++ 09-storage/exercises/02/solution/js/cookieHandler.js
@@ -1,1 +1,11 @@
+// 2. Készíts egy `cookieHandler` nevű objectet, az alábbi három metódust megvalósítja:
+// - `getAll`: kiolvassa a sütik nevét és értékét, majd visszaadja őket egy objektumban, ahol a sütik neve a key és az értéke a value (pl. { viewed: 5 })
+// - `toSessionStorage`: minden sütit egyenként elment a sessionStorage-ba az adott süti nevével és értékével
+// - `flush`: törli az összes sütit
+
+// A teszteléshez hozd létre a böngésződben az alábbi sütiket:
+// - `viewed`: 5
+// - `uid`: 354774631237
+// - `ssid`: Bx55OWbHJ0Vt_IGIF
+
 export default cookieHandler;
